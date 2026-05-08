615fa3c8df34d94537282d0a40a674e76529bbd3
rogermoniz <rotonymhb@hotmail.com>
2026-05-08 10:15:12 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,12 @@ import { PlasmicRootProvider } from "@plasmicapp/react-web";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import Link from "next/link";
-// 1. Importe le composant Script
 import Script from "next/script";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <PlasmicRootProvider Head={Head} Link={Link}>
-      {/* 2. Ajoute le script GTM ici */}
+      {/* 1. Ton script Google Tag Manager (déjà mis) */}
       <Script
         id="gtm-script"
         strategy="afterInteractive"
@@ -23,6 +22,21 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           `,
         }}
       />
+
+      {/* 2. Nouveau script Google Analytics (gtag.js) */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-S6WXWWKVQY"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-S6WXWWKVQY');
+        `}
+      </Script>
+
       <Component {...pageProps} />
     </PlasmicRootProvider>
   );
