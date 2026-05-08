bb36b69c7f7b1bc4eec957de1123109afd183096
rogermoniz <rotonymhb@hotmail.com>
2026-05-08 10:10:24 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,26 @@ import { PlasmicRootProvider } from "@plasmicapp/react-web";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import Link from "next/link";
+// 1. Importe le composant Script
+import Script from "next/script";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <PlasmicRootProvider Head={Head} Link={Link}>
+      {/* 2. Ajoute le script GTM ici */}
+      <Script
+        id="gtm-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KM8Z9NSC');
+          `,
+        }}
+      />
       <Component {...pageProps} />
     </PlasmicRootProvider>
   );
